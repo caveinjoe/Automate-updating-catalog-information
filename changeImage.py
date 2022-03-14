b3c020ac0772738ec0a9d2fc9bd8d6fdf36128ba
Kevin Jonathan <kevinjonathan1234@yahoo.com>
2022-03-14 17:17:43 +0700

    Indent fix

--- changeImage.py
+++ changeImage.py
@@ -9,15 +9,15 @@ def format_image(dir):
     for root, directory, files in os.walk(dir):
         for file in files:
             if file[0] != '.' and 'tiff' in file:
-                    og_path = os.path.join(root,file)
-                    img = Image.open(og_path)
-                    file_name = Path(og_path).stem
-                    file_path = os.path.join(root,file_name) + ".jpeg"
-                    try:
-                        img.convert("RGB").resize((600,400)).save(file_path, "JPEG")
-                        img.close()
-                    except Exception as e:
-                        print("Failure to convert {} with : {}".format(file, e))
+                og_path = os.path.join(root,file)
+                img = Image.open(og_path)
+                file_name = Path(og_path).stem
+                file_path = os.path.join(root,file_name) + ".jpeg"
+                try:
+                    img.convert("RGB").resize((600,400)).save(file_path, "JPEG")
+                    img.close()
+                except Exception as e:
+                    print("Failure to convert {} with : {}".format(file, e))
 
 
 if __name__=="__main__":
