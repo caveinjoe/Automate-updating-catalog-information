605be566a41b3c5be18de8f20cb797e1837bb50f
Kevin Jonathan <kevinjonathan1234@yahoo.com>
2022-03-14 14:37:39 +0700

    Added an empty line to report.build

--- reports.py
+++ reports.py
@@ -10,4 +10,4 @@ def generate_report(filename, title, info):
     report_title = Paragraph(title, styles["h1"])
     report_info = Paragraph(info, styles["BodyText"])
     empty_line = Spacer(1,20)
-    report.build([report_title, empty_line,report_info])
+    report.build([report_title, empty_line,report_info, empty_line])
